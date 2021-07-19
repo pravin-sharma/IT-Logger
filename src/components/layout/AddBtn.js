53f96911323b57981e0589d1becc608f1feb4f48
Pravin Sharma <pravinsharma9610@gmail.com>
2021-07-19 12:52:07 +0530

    Added Add Btn code

--- src/components/layout/AddBtn.js
+++ src/components/layout/AddBtn.js
@@ -3,7 +3,7 @@ import React from 'react'
 const AddBtn = () => {
     return (
         <div className="fixed-action-btn">
-            <a href="#add-log-modal" className="btn-floating btn-large blue darken-2">
+            <a href="#add-log-modal" className="btn-floating btn-large blue darken-2 modal-trigger">
                 <i className="large material-icons">add</i>
             </a>
             <ul>
