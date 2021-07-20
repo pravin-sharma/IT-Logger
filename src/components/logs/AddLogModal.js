c4e5a1d8041608593bf21923ab0de4b869e3ec2b
Pravin Sharma <pravinsharma9610@gmail.com>
2021-07-20 22:34:09 +0530

    Formatted code

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -4,15 +4,19 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 const AddLogModal = () => {
 
     //add states
-    const [message, setMessage] = useState('');
-    const [tech, setTech] = useState('');
+    const [message, setMessage] = useState("");
+    const [tech, setTech] = useState("");
     const [attention, setAttention] = useState(false);
 
     //onSubmit
-    const onSubmit = () => {
+    const onSubmit = (e) => {
+        e.preventDefault();
         if (message && tech) {
             M.toast({ html: 'Log Added', classes: 'rounded green' })
-            console.log(message, tech, attention);
+            //clear form
+            setMessage("");
+            setTech("");
+            setAttention(false);
         } else {
             M.toast({ html: 'Fill Message and Technician name', classes: 'rounded red' })
         }
@@ -22,24 +26,25 @@ const AddLogModal = () => {
         <div id='add-log-modal' className='modal'>
             <form>
                 <h3 className='center' style={{ marginTop: '1rem' }}>Add Log</h3>
+                
                 <div className="row">
                     <div className="input-field col s12">
-                        <i className="material-icons prefix">message</i>
-                        <input id="icon_prefix" type="text" className="validate" value={message} onChange={(e) => setMessage(e.target.value)} />
+                        <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
                         <label htmlFor="icon_prefix">Message</label>
                     </div>
                 </div>
 
                 <div className="row">
-                    <div className="input-field col s6">
-                        <i className="material-icons prefix">person</i>
-                        <select value={tech} onChange={(e) => setTech(e.target.value)}>
+                <label htmlFor='tech' className='col s6'>Technician Name</label>
+                </div>
+                <div className="row">
+                    <div className="input-field col s6" style={{marginTop:'-1rem'}}>
+                        <select name="tech" value={tech} className='browser-default' onChange={(e) => setTech(e.target.value)}>
                             <option value="" disabled>Choose your option</option>
-                            <option value="1">Option 1</option>
-                            <option value="2">Option 2</option>
-                            <option value="3">Option 3</option>
+                            <option value="1">John Doe</option>
+                            <option value="2">Sam Smith</option>
+                            <option value="3">Sara Wilson</option>
                         </select>
-                        <label>Technician Name</label>
                     </div>
                 </div>
 
@@ -48,14 +53,14 @@ const AddLogModal = () => {
                         <div style={{ marginTop: '-2rem' }}>
 
                             <label>
-                                <input type="checkbox" value={attention} onChange={() => setAttention(!attention)} />
+                                <input type="checkbox" checked={attention} onChange={() => setAttention(!attention)} />
                                 <span>Attention Need</span>
                             </label>
                         </div>
                     </div>
 
                 </div>
-                <button className="btn waves-effect waves-light right modal-close" style={{ margin: '0 1rem 1rem 0' }} onClick={onSubmit} name="action">Add
+                <button className="btn waves-effect waves-light right modal-close" style={{ margin: '0 1rem 1rem 0' }} onClick={onSubmit}>Add
                     <i className="material-icons right">add</i>
                 </button>
             </form>
